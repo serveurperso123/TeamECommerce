3ee45bd64a744e817b6e15b4b08c6de338899a6e
myris ebogo <ebogomyris@yahoo.com>
2023-02-01 18:30:21 +0100

    Ajoutez des fichiers projet.

---
+++ API/Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+﻿using Infrastructure.Data;
+using Core.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly StoreContext _context;
+
+        public ProductController(StoreContext context)
+        {
+            _context = context;
+            
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products = await _context.Products.ToListAsync();
+            return Ok(products);
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetSingleProduct(int id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+
+    }
+}
